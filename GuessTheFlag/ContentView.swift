73f631b49ee175bb303acf13ef9d9a937d2d6aa2
Pablo-Butron <Lizardlabs@icloud.com>
2021-10-03 14:33:26 -0400

    Use of Stacks

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack (){
+                Text("First")
+                Text("Second")
+                Text("Third")
+                }
+        HStack (){
+                Text("First")
+                Text("Second")
+                Text("Third")
+        }
+        HStack (){
+                Text("First")
+                Text("Second")
+                Text("Third")
+        }
     }
 }
 
